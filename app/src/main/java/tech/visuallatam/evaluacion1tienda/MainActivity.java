15abfec0a99d09b686aefc50d72464d1b4a5e699
Donali <00084417@uca.edu.sv>
2019-03-29 13:39:36 -0600

    Initial commit

---
+++ app/src/main/java/tech/visuallatam/evaluacion1tienda/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.visuallatam.evaluacion1tienda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
